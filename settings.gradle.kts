249715b1a3ce27484637109e513cb4df0cfc408a
jjjlyn <prize1142@gmail.com>
2021-04-16 14:35:02 +0900

    #8 chore: 모듈 등록

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
+include(":feature:main")
+include(":model")
+include(":core")
+include(":data:db")
+include(":data:repository")
+include(":data:api")
+include(":data")
 include(":app")
 rootProject.name = "IceCrush"
