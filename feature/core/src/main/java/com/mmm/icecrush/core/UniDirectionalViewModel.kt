088c1b27192928b7ae209b23438cf1839ed2be50
jjjlyn <prize1142@gmail.com>
2021-04-18 17:09:27 +0900

    #10 feat(core): 공통 ViewModel 추가

--- feature/core/src/main/java/com/mmm/icecrush/core/UniDirectionalViewModel.kt
+++ feature/core/src/main/java/com/mmm/icecrush/core/UniDirectionalViewModel.kt
@@ -1,7 +1,33 @@
 package com.mmm.icecrush.core
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+
+data class StateEffectDispatch<STATE, EFFECT, EVENT>(
+    val state: STATE,
+    val effectFlow: Flow<EFFECT>,
+    val dispatch: (EVENT) -> Unit
+)
+
+@Composable
+inline fun <reified EVENT, EFFECT, STATE> use(
+    viewModel: UniDirectionalViewModel<EVENT, EFFECT, STATE>
+) : StateEffectDispatch<STATE, EFFECT, EVENT> {
+    val state by viewModel.state.collectAsState()
+
+    val dispatch: (EVENT) -> Unit = { event ->
+        viewModel.event(event)
+    }
+    return StateEffectDispatch(
+        state = state,
+        effectFlow = viewModel.effect,
+        dispatch = dispatch
+    )
+}
+
 
 interface UniDirectionalViewModel<EVENT, EFFECT, STATE> {
     val state: StateFlow<STATE>
